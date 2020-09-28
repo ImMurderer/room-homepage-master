f12c4a55691e06a0fd0833a9411a7492b71c60b9
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-09-28 16:55:05 -0300

    done slides logic

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StyledContent from './styles';
 
 import ImageHeroOne from '../../../images/desktop-image-hero-1.jpg';
+import ImageHeroTwo from '../../../images/desktop-image-hero-2.jpg';
+import ImageHeroThree from '../../../images/desktop-image-hero-3.jpg';
 import IconArrow from '../../../images/icon-arrow.svg';
 import IconAngleLeft from '../../../images/icon-angle-left.svg';
 import IconAngleRight from '../../../images/icon-angle-right.svg';
 
+const slides = [
+    {
+        img: ImageHeroOne,
+        title: 'Discover innovative ways to decorate',
+        text: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
+    }, {
+        img: ImageHeroTwo,
+        title: 'We are available all across the globe',
+        text: 'With stores all over the world, it\'s easy for you to find furniture for your home or place of business. Locally, we\'re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don\'t hesitate to contact us today.'
+    }, {
+        img: ImageHeroThree,
+        title: 'Manufactured with the best materials',
+        text: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
+    }
+]
 
 const Home = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleClick = (n) => {
+        setCurrentSlide((currentSlide + n + 3) % 3);
+    }
+
     return (
         <StyledContent>
-            <img src={ImageHeroOne} />
+            <img src={slides[currentSlide].img} />
             <div>
-                <h1>Discover innovative ways to decorate</h1>
+                <h1>{slides[currentSlide].title}</h1>
                 <p>
-
-                    We provide unmatched quality, comfort, and style for property owners across the country.
-                    Our experts combine form and function in bringing your vision to life. Create a room in your
-                    own style with our collection and make your property a reflection of you and what you love.
+                    {slides[currentSlide].text}
                     <span>
                         <a>
                             Shop now {' '}
@@ -26,8 +46,8 @@ const Home = () => {
                     </span>
                 </p>
                 <div className="control-buttons">
-                    <button><img src={IconAngleLeft} /></button>
-                    <button><img src={IconAngleRight} /></button>
+                    <button onClick={() => handleClick(-1)}><img src={IconAngleLeft} /></button>
+                    <button onClick={() => handleClick(1)}><img src={IconAngleRight} /></button>
                 </div>
             </div>
         </StyledContent>
